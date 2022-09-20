70980e112bedcdcc923bba7d966d5e9f151797da
seojaeoh <seojaeohcode@gmail.com>
2022-09-20 15:38:19 +0900

    commit

--- d0920/stuPjt/stuPjt/urls.py
+++ d0920/stuPjt/stuPjt/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('students/',include('students.urls')),
 ]
--- d0920/stuPjt/students/views.py
+++ d0920/stuPjt/students/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def stuWrite(request):
+    return render(request, 'stuWrite.html')
+
+def board(request):
+    return render(request, 'board.html')
