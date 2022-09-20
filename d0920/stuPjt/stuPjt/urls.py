b577a14c6558d6353f5af775b4ea39f4715c4a82
seojaeoh <seojaeohcode@gmail.com>
2022-09-20 15:11:50 +0900

    commit

---
+++ d0920/stuPjt/stuPjt/urls.py
@@ -0,0 +1,9 @@
+import imp
+from turtle import home
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('home.urls')),
+]
